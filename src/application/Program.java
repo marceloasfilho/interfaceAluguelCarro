e8f6050efe264290b46692ccb5b63efd85a31ef3
Marcelo Filho <marceloandrade_silva@hotmail.com>
2021-10-17 20:09:32 -0300

    Exemplo de utilização de interfaces para aluguel de carro

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.Aluguel;
+import services.BrasilTaxaServico;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Aluguel aluguel = new Aluguel(10.00, 130.00, new BrasilTaxaServico());
+
+    }
+}
